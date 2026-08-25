a3fd50d5b0670f21f796ed66c1baf98190dfea03
Tatiana Ivanova <tatiana_ivanova@epam.com>
2026-08-25 17:27:13 +0300

    Array Task Part 2: code refactoring

--- src/main/java/com/innowise/jck/arraytask/util/IdGenerator.java
+++ src/main/java/com/innowise/jck/arraytask/util/IdGenerator.java
@@ -2,16 +2,15 @@ package com.innowise.jck.arraytask.util;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import java.util.concurrent.atomic.AtomicLong;
 
 public class IdGenerator {
   private static final Logger logger = LogManager.getLogger(IdGenerator.class);
-  private static final AtomicLong counter = new AtomicLong(1);
+  private static long counter = 1L;
 
   private IdGenerator() {}
 
   public static String generateId() {
-    String id = String.valueOf(counter.getAndIncrement());
+    String id = String.valueOf(counter++);
     logger.debug("Generated unique ID: {}", id);
     return id;
   }
